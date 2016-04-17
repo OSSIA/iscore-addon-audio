513df589678737ab38ad576b05807d70cf997926
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-04-17 23:55:16 +0200

    add routing serialization

--- Audio/MixProcess/MixProcessModelSerialization.cpp
+++ Audio/MixProcess/MixProcessModelSerialization.cpp
@@ -1,6 +1,40 @@
 #include "MixProcessModel.hpp"
 #include <iscore/plugins/documentdelegate/plugin/ElementPluginModelList.hpp>
 
+
+template<>
+void Visitor<Reader<DataStream>>::readFrom_impl(const Audio::Mix::Routing& proc)
+{
+    m_stream << proc.in << proc.out << proc.mix;
+
+    insertDelimiter();
+}
+
+template<>
+void Visitor<Writer<DataStream>>::writeTo(Audio::Mix::Routing& proc)
+{
+    m_stream >> proc.in >> proc.out >> proc.mix;
+
+    checkDelimiter();
+}
+
+template<>
+void Visitor<Reader<JSONObject>>::readFrom_impl(const Audio::Mix::Routing& proc)
+{
+    m_obj["In"] = toJsonValue(proc.in);
+    m_obj["Out"] = toJsonValue(proc.out);
+    m_obj["Mix"] = proc.mix;
+}
+
+template<>
+void Visitor<Writer<JSONObject>>::writeTo(Audio::Mix::Routing& proc)
+{
+    proc.in = fromJsonValue<Id<Process::ProcessModel>>(m_obj["In"]);
+    proc.out = fromJsonValue<Id<Process::ProcessModel>>(m_obj["Out"]);
+    proc.mix = m_obj["Mix"].toDouble();
+}
+
+
 template<>
 void Visitor<Reader<DataStream>>::readFrom_impl(const Audio::Mix::ProcessModel& proc)
 {
