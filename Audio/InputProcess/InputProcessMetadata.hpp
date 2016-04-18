71fb6339d64dfd38e1afaf6f68de21b645ffdb5b
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-04-18 16:27:34 +0200

    Add an input process

---
+++ Audio/InputProcess/InputProcessMetadata.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <Process/ProcessMetadata.hpp>
+#include <iscore_plugin_audio_export.h>
+
+namespace Process
+{
+class ProcessFactory;
+}
+
+namespace Audio
+{
+namespace Input
+{
+class ProcessModel;
+}
+}
+
+PROCESS_METADATA(
+        ISCORE_PLUGIN_AUDIO_EXPORT,
+        Audio::Input::ProcessModel,
+        "89963256-8e47-4590-bc72-522040f2fa1c",
+        "Input",
+        "Input"
+        )
+
+
+UNDO_NAME_METADATA(EMPTY_MACRO, Audio::Input::ProcessModel, "Input")
---
+++ Audio/InputProcess/InputProcessModel.hpp
@@ -0,0 +1,93 @@
+#pragma once
+#include <Process/Process.hpp>
+#include <iscore/serialization/DataStreamVisitor.hpp>
+#include <iscore/serialization/JSONVisitor.hpp>
+#include <iscore/serialization/VisitorCommon.hpp>
+#include <Audio/InputProcess/InputProcessMetadata.hpp>
+#include <DummyProcess/DummyLayerPanelProxy.hpp>
+#include <Process/LayerModel.hpp>
+#include <Audio/MediaFileHandle.hpp>
+#include <unordered_set>
+
+#include <boost/multi_index/identity.hpp>
+
+namespace Audio
+{
+namespace Input
+{
+class ProcessModel final :
+        public Process::ProcessModel,
+        public Nano::Observer
+{
+        ISCORE_SERIALIZE_FRIENDS(Audio::Input::ProcessModel, DataStream)
+        ISCORE_SERIALIZE_FRIENDS(Audio::Input::ProcessModel, JSONObject)
+
+        Q_OBJECT
+    public:
+        explicit ProcessModel(
+                const TimeValue& duration,
+                const Id<Process::ProcessModel>& id,
+                QObject* parent);
+
+        explicit ProcessModel(
+                const ProcessModel& source,
+                const Id<Process::ProcessModel>& id,
+                QObject* parent);
+
+        virtual ~ProcessModel();
+
+        template<typename Impl>
+        explicit ProcessModel(
+                Deserializer<Impl>& vis,
+                QObject* parent) :
+            Process::ProcessModel{vis, parent}
+        {
+            vis.writeTo(*this);
+        }
+
+        // Process interface
+        ProcessModel* clone(
+                const Id<Process::ProcessModel>& newId,
+                QObject* newParent) const override;
+
+        UuidKey<Process::ProcessFactory> concreteFactoryKey() const override
+        {
+            return Metadata<ConcreteFactoryKey_k, Audio::Input::ProcessModel>::get();
+        }
+
+        QString prettyName() const override;
+        QByteArray makeLayerConstructionData() const override;
+
+        void setDurationAndScale(const TimeValue& newDuration) override;
+        void setDurationAndGrow(const TimeValue& newDuration) override;
+        void setDurationAndShrink(const TimeValue& newDuration) override;
+
+        void startExecution() override;
+        void stopExecution() override;
+        void reset() override;
+
+        ProcessStateDataInterface* startStateData() const override;
+        ProcessStateDataInterface* endStateData() const override;
+
+        Selection selectableChildren() const override;
+        Selection selectedChildren() const override;
+        void setSelection(const Selection& s) const override;
+
+        void serialize_impl(const VisitorVariant& vis) const override;
+
+    signals:
+        void routingChanged();
+
+    protected:
+        Process::LayerModel* makeLayer_impl(
+                const Id<Process::LayerModel>& viewModelId,
+                const QByteArray& constructionData,
+                QObject* parent) override;
+        Process::LayerModel* loadLayer_impl(const VisitorVariant&, QObject* parent) override;
+        Process::LayerModel* cloneLayer_impl(const Id<Process::LayerModel>& newId, const Process::LayerModel& source, QObject* parent) override;
+
+    private:
+
+};
+}
+}
