0d3bea56096b2849792b59d5f6194870141571d9
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-04-20 10:44:33 +0200

    work on tests

--- tests/test2.cpp
+++ tests/test2.cpp
@@ -5,6 +5,7 @@
 #include <QObject>
 #include <thread>
 #include <chrono>
+#include <iostream>
 using namespace std::chrono_literals;
 
 class test1: public QObject
@@ -40,7 +41,11 @@ class test1: public QObject
             m_ctx.player = OpenAudioPlayer(2, 2, 44100, 512, 65536*4, 44100*60*20, renderer, 1);
             m_ctx.renderer = GetAudioPlayerRenderer(m_ctx.player);
 
+            std::cout << std::flush;
+            std::cerr << std::flush;
             // Build the first stream
+            auto stream1 = MakeReadSound("/tmp/1.wav");
+            /*
             auto stream1 = [] {
                 auto player = MakeGroupPlayer();
                 auto date1 = GenRealDate(player, 44100);
@@ -65,27 +70,33 @@ class test1: public QObject
                 // Make a stream on the sound
                 return MakeGroupStream(player);
             }();
+            */
+
+            auto fx_send = MakeSend(stream1);
 
             // Build a first effect stream
             {
-                auto fx_send_1 = MakeSend(stream1);
+                /*
                 auto fx_1 = MakeFaustAudioEffect("/tmp/examples/freeverb.dsp", "/usr/local/lib/faust/architecture", "");
-                auto fx_return_1 = MakeReturn(fx_send_1);
+                auto fx_return_1 = MakeReturn(fx_send);
                 auto fx_chain_1 = MakeEffectSound(fx_return_1, fx_1, 0, 0);
 
                 StartSound(m_ctx.player, fx_chain_1, GenRealDate(m_ctx.player, 0));
+                */
             }
 
             // Build a second effect stream
             {
-                auto fx_send_2 = MakeSend(stream1);
-                auto fx_2 = MakeFaustAudioEffect("/tmp/examples/freeverb.dsp", "/usr/local/lib/faust/architecture", "");
-                auto fx_return_2 = MakeReturn(fx_send_2);
+                auto fx_2 = MakeFaustAudioEffect("/tmp/examples/noise.dsp", "/usr/local/lib/faust/architecture/", "");
+                auto fx_return_2 = MakeReturn(fx_send);
                 auto fx_chain_2 = MakeEffectSound(fx_return_2, fx_2, 0, 0);
 
                 StartSound(m_ctx.player, fx_chain_2, GenRealDate(m_ctx.player, 0));
             }
 
+            std::cout << std::flush;
+            std::cerr << std::flush;
+            StartSound(m_ctx.player, fx_send, GenRealDate(m_ctx.player, 0));
             // Mix the streams
             StartAudioPlayer(m_ctx.player);
 
