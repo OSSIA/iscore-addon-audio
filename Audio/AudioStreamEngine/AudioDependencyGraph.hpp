d125ac873d8e3fcdf65c0363a0fb4f707e14a1bf
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-05-12 17:27:59 +0200

    refactor

---
+++ Audio/AudioStreamEngine/AudioDependencyGraph.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+
+#include <boost/graph/adjacency_list.hpp>
+
+#include <boost/graph/breadth_first_search.hpp>
+#include <boost/graph/graphviz.hpp>
+#include <boost/graph/labeled_graph.hpp>
+#include <boost/graph/topological_sort.hpp>
+
+namespace Process
+{
+class ProcessModel;
+}
+namespace Audio
+{
+namespace Send
+{
+class ProcessModel;
+}
+namespace Effect
+{
+class ProcessModel;
+}
+namespace AudioStreamEngine
+{
+class Context;
+class ConstraintComponent;
+class SendComponent;
+class EffectProcessComponent;
+class ReturnComponent;
+class SoundComponent;
+class ScenarioComponent;
+class LoopComponent;
+
+struct AudioDependencyGraph
+{
+        using node_t = eggs::variant<
+            ConstraintComponent*,
+            ScenarioComponent*,
+            EffectProcessComponent*,
+            LoopComponent*,
+            SendComponent*,
+            ReturnComponent*,
+            SoundComponent*
+        >;
+
+        boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, node_t> m_graph;
+
+        using vtx_t = decltype(boost::add_vertex(node_t{}, m_graph));
+
+    public:
+        SendComponent* getComponent(const Send::ProcessModel& proc);
+
+        AudioDependencyGraph(ConstraintComponent& root);
+
+        boost::optional<std::deque<int>> check() const;
+        void apply(const std::deque<int>&, Context& ctx);
+
+    private:
+        vtx_t visit(ConstraintComponent& cst);
+        vtx_t visit(ScenarioComponent& proc);
+        vtx_t visit(EffectProcessComponent& proc);
+        vtx_t visit(LoopComponent& proc);
+        vtx_t visit(SendComponent& proc);
+        vtx_t visit(ReturnComponent& proc);
+        vtx_t visit(SoundComponent& proc);
+
+    private:
+};
+
+}
+}
