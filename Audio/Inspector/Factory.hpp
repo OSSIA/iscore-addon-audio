c1d5a936d9b120b1b6cb25842a4d160f08c123c2
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-08-18 14:43:07 +0200

    simplify

--- Audio/Inspector/Factory.hpp
+++ Audio/Inspector/Factory.hpp
@@ -1,81 +1,51 @@
 #pragma once
 #include <Process/Inspector/ProcessInspectorWidgetDelegateFactory.hpp>
 
+#include <Audio/SoundProcess/SoundProcessModel.hpp>
+#include <Audio/MixProcess/MixProcessModel.hpp>
+#include <Audio/ReturnProcess/ReturnProcessModel.hpp>
+#include <Audio/EffectProcess/EffectProcessModel.hpp>
+
+#include <Audio/Inspector/AudioInspector.hpp>
+#include <Audio/Inspector/MixInspector.hpp>
+#include <Audio/Inspector/ReturnInspector.hpp>
+#include <Audio/Inspector/EffectInspector.hpp>
+
 namespace Audio
 {
 namespace Sound
 {
 class InspectorFactory final :
-        public Process::InspectorWidgetDelegateFactory
+        public Process::InspectorWidgetDelegateFactory_T<ProcessModel, InspectorWidget>
 {
         ISCORE_CONCRETE_FACTORY("10360b4c-4d73-4274-8339-408644408cab")
-    public:
-        InspectorFactory();
-        virtual ~InspectorFactory();
-
-    private:
-        Process::InspectorWidgetDelegate* make(
-                const Process::ProcessModel&,
-                const iscore::DocumentContext&,
-                QWidget* parent) const override;
-        bool matches(const Process::ProcessModel&) const override;
 };
 }
 
 namespace Mix
 {
 class InspectorFactory final :
-        public Process::InspectorWidgetDelegateFactory
+        public Process::InspectorWidgetDelegateFactory_T<ProcessModel, InspectorWidget>
 {
         ISCORE_CONCRETE_FACTORY("60efbf22-8374-4923-8a07-8caa4d242954")
-    public:
-        InspectorFactory();
-        virtual ~InspectorFactory();
-
-    private:
-        Process::InspectorWidgetDelegate* make(
-                const Process::ProcessModel&,
-                const iscore::DocumentContext&,
-                QWidget* parent) const override;
-        bool matches(const Process::ProcessModel&) const override;
 };
 }
 
 namespace Return
 {
 class InspectorFactory final :
-        public Process::InspectorWidgetDelegateFactory
+        public Process::InspectorWidgetDelegateFactory_T<ProcessModel, InspectorWidget>
 {
         ISCORE_CONCRETE_FACTORY("a5afd64d-750b-47c4-90ea-9788f47df9b8")
-    public:
-        InspectorFactory();
-        virtual ~InspectorFactory();
-
-    private:
-        Process::InspectorWidgetDelegate* make(
-                const Process::ProcessModel&,
-                const iscore::DocumentContext&,
-                QWidget* parent) const override;
-        bool matches(const Process::ProcessModel&) const override;
 };
 }
 
 namespace Effect
 {
 class InspectorFactory final :
-        public Process::InspectorWidgetDelegateFactory
+        public Process::InspectorWidgetDelegateFactory_T<ProcessModel, InspectorWidget>
 {
         ISCORE_CONCRETE_FACTORY("cc8ceff3-ef93-4b73-865a-a9f870d6e898")
-    public:
-        InspectorFactory();
-        virtual ~InspectorFactory();
-
-    private:
-        Process::InspectorWidgetDelegate* make(
-                const Process::ProcessModel&,
-                const iscore::DocumentContext&,
-                QWidget* parent) const override;
-        bool matches(const Process::ProcessModel&) const override;
 };
 }
 }
