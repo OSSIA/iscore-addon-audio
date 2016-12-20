5935bc5d450a4f828e3b965f8dc5311d5d8d5cd7
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-12-20 22:53:12 +0100

    refactor

--- Audio/AudioStreamEngine/AudioComponent.hpp
+++ Audio/AudioStreamEngine/AudioComponent.hpp
@@ -9,10 +9,10 @@ namespace Audio
 namespace AudioStreamEngine
 {
 
-class Component : public iscore::SystemComponent<iscore::Component, DocumentPlugin>
+class Component : public iscore::GenericComponent<DocumentPlugin>
 {
     public:
-        using iscore::SystemComponent<iscore::Component, DocumentPlugin>::SystemComponent;
+        using iscore::GenericComponent<DocumentPlugin>::GenericComponent;
         virtual ~Component();
         virtual optional<AudioGraphVertice> visit(AudioGraph& graph) = 0;
         virtual void makeStream(const Context& player) = 0;
